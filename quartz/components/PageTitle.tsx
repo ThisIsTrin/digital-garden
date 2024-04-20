85cf72177491149d7a38cf08cc29792b99897efc
ThisIsTrin <chaloton1610@gmail.com>
2024-04-20 16:44:26 +0700

    Quartz sync: Apr 20, 2024, 4:44 PM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -9,7 +9,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   return (
     <h1 class={classNames(displayClass, "page-title")}>
       <a href={baseDir}>
-        <img src="/static/icon.png" />
+        <img src="/digital-garden/static/icon.png" />
         {title}
       </a>
     </h1>
